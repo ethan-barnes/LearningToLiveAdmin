a852f36050fe8069e0410c7dd81bf0b864f592f3
Ethan Barnes <ecbarnes@bradford.ac.uk>
2022-02-06 13:29:55 +0000

    Added firebase page

--- L2LAdminPage/Controllers/HomeController.cs
+++ L2LAdminPage/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace L2LAdminPage.Controllers
             return View();
         }
 
+        public IActionResult Firebase()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
