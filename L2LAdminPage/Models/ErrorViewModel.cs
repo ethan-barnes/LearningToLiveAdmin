99d8ecf874e02ef992bc97e2cadc0196b9798951
Ethan Barnes <ecbarnes@bradford.ac.uk>
2022-02-06 13:25:07 +0000

    Add project files.

---
+++ L2LAdminPage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace L2LAdminPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
