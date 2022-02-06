cc0273e886e55b07fbee72e84795e541b42e63a2
Ethan Barnes <ecbarnes@bradford.ac.uk>
2022-02-06 15:02:08 +0000

    Added firebase request controller

---
+++ L2LAdminPage/Controllers/FirebaseController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace L2LAdminPage.Controllers
+{
+    public class FirebaseController : Controller
+    {
+        // Get: /Firebase/
+        public string Index()
+        {
+            return "This is my default action.";
+        }
+
+        // Get: /Firebase/GetJson
+        // Get: /Firebase/GetJson?url=https://www.placeholder.com
+        public string GetJson(string url)
+        {
+            try
+            {
+                return Get(url);
+            } 
+            catch (Exception e)
+            {
+                return $"Error occured: {e}";
+            }
+            
+        }
+
+        private string Get(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
--- L2LAdminPage/wwwroot/js/site.js
+++ L2LAdminPage/wwwroot/js/site.js
@@ -1,7 +1,18 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
-function logIn() {
-    window.location.replace("https://www.google.com/");
+
+function FirebaseGet() {
+    var myUrl = 'https://learningtolive-e4844-default-rtdb.europe-west1.firebasedatabase.app/.json'
+    $.ajax({
+        url: 'https://localhost:44369/Firebase/GetJson?url=' + myUrl,
+        success: function (data) {
+            console.log(data);
+            document.getElementById("firebaseText").innerHTML = data;
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            //some errror, some show err msg to user and log the error  
+            alert(xhr.responseText);
+        }
+    });
 }
