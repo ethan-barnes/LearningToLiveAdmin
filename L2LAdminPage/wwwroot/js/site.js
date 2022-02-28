0f8574cf16a198d3c144d26d54c016cf2a41a656
Ethan Barnes <barnes.c.ethan@gmail.com>
2022-02-28 12:55:41 +0000

    Processing firebase data

--- L2LAdminPage/wwwroot/js/site.js
+++ L2LAdminPage/wwwroot/js/site.js
@@ -3,16 +3,63 @@
 
 
 function FirebaseGet() {
-    var myUrl = 'https://learningtolive-e4844-default-rtdb.europe-west1.firebasedatabase.app/.json'
+    var dbUrl = 'https://learningtolive-e4844-default-rtdb.europe-west1.firebasedatabase.app/.json'
+    var siteUrl = window.location.host;
     $.ajax({
-        url: 'https://localhost:44369/Firebase/GetJson?url=' + myUrl,
+        url: 'https://' + siteUrl + '/Firebase/GetJson?url=' + dbUrl,
         success: function (data) {
-            console.log(data);
-            document.getElementById("firebaseText").innerHTML = data;
+            displayFirebaseData(data);
         },
         error: function (xhr, ajaxOptions, thrownError) {
             //some errror, some show err msg to user and log the error  
-            alert(xhr.responseText);
+            alert("Error reading firebase");
+            console.log(xhr);
         }
     });
+}
+
+function displayFirebaseData(fbData) {
+    // Prevents multiple button presses showing multiple data.
+    document.getElementById("firebaseElements").innerHTML = ""; 
+
+    var textToDisplay = "";
+    var json = JSON.parse(fbData);
+    console.log(json);
+
+    for (var key in json) {
+        if (json.hasOwnProperty(key)) {
+            console.log(key + " -> " + json[key]);
+
+            // Create heading for each key in database
+            var h3 = document.createElement("h3");
+            var node = document.createTextNode(capitaliseFirstLetter(key));
+
+            h3.appendChild(node);
+            var div = document.getElementById("firebaseElements");
+            div.appendChild(h3);
+
+            for (var child in json[key]) {
+                // Create heading for children of key
+                var h5 = document.createElement("h5");
+                var node1 = document.createTextNode(child.toString());
+
+                h5.appendChild(node1);
+                div.appendChild(h5);
+                for (var child2 in child) {
+                    var h7 = document.createElement("h7");
+                    var node1 = document.createTextNode(child2.toString()); // TODO fix this
+
+                    h7.appendChild(node1);
+                    div.appendChild(h7);
+                }
+            }
+        }
+    }
+
+    //document.getElementById("firebaseText").innerHTML = textToDisplay;
+
+}
+
+function capitaliseFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
